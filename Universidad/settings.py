a2ede5094e75796b6ec901b22fc69fdd3c3f2511
Franco <Francosk2017@gmail.com>
2025-05-28 14:22:43 -0300

    Actualizar settings.py y agregar Procfile y requirements.txt para deploy

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pjc=co6ykbzkp@sip6xw!)8t2$8!26g6)9t#hq_vwi()bs5d$u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-pjc=co6ykbzkp@sip6xw!)8t2$8!26g6)9t#hq_vwi()bs5d$u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,12 +78,11 @@ WSGI_APPLICATION = 'Universidad.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Universidad',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'DATABASE_PORT': '5432',
-
+        'NAME': os.environ.get('DB_NAME', 'Universidad'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
