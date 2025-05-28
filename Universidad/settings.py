db9106c864058de3d1739b221e101a0ec7594aba
Franco <Francosk2017@gmail.com>
2025-05-28 15:01:41 -0300

    Agregar dominio Render a ALLOWED_HOSTS

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-pjc=co6ykbzkp@sip6xw!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gymmaxpower-github-io.onrender.com']
 
 
 # Application definition
